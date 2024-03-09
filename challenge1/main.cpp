c41d48d50225c786f7db379043178558a16883b4
Henrik Bang-Olsen <henrikbang01@gmail.com>
2024-03-09 17:48:30 +0000

    simple working Makefile for compiling and clearing

--- challenge1/main.cpp
+++ challenge1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using Vec = std::vector<double>;
 
@@ -13,7 +14,27 @@ struct parameters
     double sigma; // for Armijo rule
 };
 
+double norm(const Vec &v)
+{
+    double sum = 0.0;
+    for (const auto &elm : v)
+    {
+        sum += elm * elm;
+    }
+    return std::sqrt(sum);
+}
+
 int main()
 {
+    Vec vec1 = {2.0, -2.0};
+
+    for (double i : vec1)
+    {
+        std::cout << i << "\n";
+    }
+
+    double norm1 = norm(vec1);
+    std::cout << norm1;
+
     return 0;
 }
