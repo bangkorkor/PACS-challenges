fa8b7966fc87655b2b716c83ce48118f8e1cd50b
Henrik Bang-Olsen <henrikbang01@gmail.com>
2024-03-11 14:52:21 +0000

    Added other StepSize methods, user is now asked what method to use. Still some flaws in code

--- challenge1/src/gradientMethod.cpp
+++ challenge1/src/gradientMethod.cpp
@@ -17,6 +17,13 @@ struct Parameters
     double sigma; // for Armijo rule
 };
 
+enum class StepSizeStrategy
+{
+    Armijo,
+    Exponential,
+    Inverse
+};
+
 // --- gets the norm of a input vector
 double norm(const Vec &v)
 {
@@ -28,6 +35,7 @@ double norm(const Vec &v)
     return std::sqrt(sum);
 }
 
+// --- uses the Armijo rule to compute the decay of alpha
 double armijo(const Parameters &params, Func &f, const Vec &grad_f, const Vec &xk)
 {
     double alpha = params.a0;
@@ -46,14 +54,39 @@ double armijo(const Parameters &params, Func &f, const Vec &grad_f, const Vec &x
     return alpha;
 }
 
-Vec gradientDecent(Func f, GradFunc grad_f, const Parameters &params)
+// --- uses the exponential decay to compute the decay of alpha
+double exponentialDecay(const Parameters &params, int k)
+{
+    return params.a0 * std::exp(-0.01 * k);
+}
+
+// --- uses the inverse decay to compute the decay of alpha
+double inverseDecay(const Parameters &params, int k)
+{
+    return params.a0 / (1 + 0.01 * k);
+}
+
+// --- computes the gradient descent
+Vec gradientDecent(Func f, GradFunc grad_f, const Parameters &params, const int strategy)
 {
     Vec xk = params.x0;
+    double alpha = params.a0;
     int k = 0; // number of iterations
     while (k < params.kmax)
     {
         Vec grad = grad_f(xk); // grad = ∇f(xk)
-        double alpha = armijo(params, f, grad, xk);
+        switch (strategy)
+        {
+        case 1:
+            alpha = armijo(params, f, grad, xk);
+            break;
+        case 2:
+            alpha = exponentialDecay(params, k);
+            break;
+        case 3:
+            alpha = inverseDecay(params, k);
+            break;
+        }
 
         Vec x_next(xk.size());
         for (int i = 0; i < xk.size(); ++i)
@@ -79,35 +112,41 @@ Vec gradientDecent(Func f, GradFunc grad_f, const Parameters &params)
 
 int main()
 {
+
+    int chooseStrategy;
+    while (true)
+    {
+        std::cout << "Choose the strategy for the step size: \n";
+        std::cout << "1. Armijo\n";
+        std::cout << "2. Exponential\n";
+        std::cout << "3. Inverse\n";
+        std::cin >> chooseStrategy;
+        if (chooseStrategy == 1 || chooseStrategy == 2 || chooseStrategy == 3)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid input, please try again\n";
+        }
+    }
+
     Parameters params = {{0, 0}, 0.01, 1e-6, 1e-6, 1000, 0.45}; // {{x0}, a0, eps_s, eps_r, kmax, sigma}
-
-    // --- simple example f(x) = x^2 - 3x + 2
-    // Func g = [](const Vec &x) -> double
-    // {
-    //     return x[0] * x[0] - 3 * x[0] + 2;
-    // };
-
-    // GradFunc grad_g = [](const Vec &x) -> Vec
-    // {
-    //     return {2 * x[0] - 3}; // gradF = {2x -3}
-    // };
-
-    // Vec min_point_g = gradientDecent(g, grad_g, params);
-    // std::cout << "Minimum found at: (" << min_point_g[0] << ")\n";
-    // std::cout << "Function value at minimum: " << g(min_point_g) << std::endl;
 
     // --- test case
     Func f = [](const Vec &x) -> double
     {
-        return x[0] * x[1] + 4 * x[0] * x[0] * x[0] * x[0] + x[1] * x[1] + 3 * x[0]; // f(x,y) = xy + 4*x^4 + y^2 + 3*x
+        return x[0] * x[1] + 4 * pow(x[0], 4) + pow(x[1], 2) + 3 * x[0]; // f(x,y) = x*y + 4*x^4 + y^2 + 3*x
     };
 
     GradFunc grad_f = [](const Vec &x) -> Vec
     {
-        return {x[1] + 16 * x[0] * x[0] * x[0] + 3, x[0] + 2 * x[1]}; // gradF = {y + 16*x^3 + 3, x + 2*y}
+        return {16 * pow(x[0], 3) + x[1] + 3, x[0] + 2 * x[1]}; // gradF = {y + 16*x^3 + 3, x + 2*y}
     };
 
-    Vec min_point_f = gradientDecent(f, grad_f, params);
+    Vec min_point_f = gradientDecent(f, grad_f, params, chooseStrategy);
+    std::cout << "---------\n";
+    std::cout << "Using strategy " << chooseStrategy << "\n";
     std::cout << "Minimum found at: (" << min_point_f[0] << ", " << min_point_f[1] << ")\n";
     std::cout << "Function value at minimum: " << f(min_point_f) << std::endl;
 
